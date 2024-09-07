ffb31ce5b77818935e559e43016922358fc9747f
ghluka <ghluka@users.noreply.github.com>
2024-09-07 16:33:21 +0000

    deploy: 3353ed8049c1fdfa7ae1fb8f7a2e82d163434dea

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-Mine Parkour-0.0.29";
+const cacheName = "DefaultCompany-Mine Parkour-0.0.30";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js",
