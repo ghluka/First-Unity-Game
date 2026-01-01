1dcbb3a8771cf2c8298c485f69c62dd342fd50cc
Luka <164436043+ghluka@users.noreply.github.com>
2026-01-01 02:08:21 -0500

    raw mouse input?

--- src/Assets/Scripts/World/Player/MouseMovement.cs
+++ src/Assets/Scripts/World/Player/MouseMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseMovement : MonoBehaviour
 {
     float xRotation = 20f;
-    float YRotation = 0f;
+    float yRotation = 0f;
 
     void Start()
     {
@@ -14,19 +14,16 @@ public class MouseMovement : MonoBehaviour
 
     void Update()
     {
-        float mouseSensitivity = PlayerPrefs.GetFloat("mouseSensitivity");
-        if (mouseSensitivity == 0) {
-            PlayerPrefs.SetFloat("mouseSensitivity", .5f);
-            mouseSensitivity = .5f;
-        }
+        float mouseSensitivity = PlayerPrefs.GetFloat("mouseSensitivity", 0.5f);
 
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 100f * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 100f * Time.deltaTime;
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * 100f;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * 100f;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -89.99f, 89.99f);
-        YRotation += mouseX;
 
-        transform.localRotation = Quaternion.Euler(xRotation, YRotation, 0f);
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
